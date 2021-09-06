17a312dc65c1b1fed7ace508c3d938b2c7192f69
pravin5551 <pravinvpawar5551@gmail.com>
2021-09-06 12:20:41 +0530

    Constant dart file created

---
+++ udemy_pract/lib/constants.dart
@@ -0,0 +1,7 @@
+
+class Constants {
+  static const String textSting ='''Hello World! This is first page opf our container,
+                   backgroundColor: Colors.red,Hello World! 
+                   This is first page opf our container, 
+                   backgroundColor: Colors.red,''';
+}
